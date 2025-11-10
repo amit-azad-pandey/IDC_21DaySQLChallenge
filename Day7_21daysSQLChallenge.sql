0abd0a7cc665fdc8688fc0856e3fc00aede9aa4d
amit pandey <35841970+amit-azad-pandey@users.noreply.github.com>
2025-11-10 21:45:15 +0530

    Add files via upload

---
+++ Day7_21daysSQLChallenge.sql
@@ -0,0 +1,25 @@
+--Practice Questions:
+-- 1. Find services that have admitted more than 500 patients in total.
+SELECT service, SUM(patients_admitted) AS total_patients_admitted
+FROM services_weekly
+GROUP BY service
+HAVING SUM(patients_admitted) > 500;
+
+-- 2. Show services where average patient satisfaction is below 75.
+SELECT service, AVG(satisfaction) AS avg_patient_satisfaction
+FROM patients
+GROUP BY service
+HAVING AVG(satisfaction) < 75;
+
+-- 3. List weeks where total staff presence across all services was less than 50.
+SELECT week, SUM(staff_morale) AS total_staff_presence
+FROM services_weekly
+GROUP BY week
+HAVING SUM(staff_morale) < 50;
+
+-- Daily Challenge:
+-- Question: Identify services that refused more than 100 patients in total and had an average patient satisfaction below 80. Show service name, total refused, and average satisfaction.
+SELECT service, SUM(patients_refused) AS total_refused, ROUND(AVG(patient_satisfaction), 0) AS total_patients_satisfaction
+FROM services_weekly
+GROUP BY service
+HAVING SUM(patients_refused) > 100 AND ROUND(AVG(patient_satisfaction), 0) < 80;
