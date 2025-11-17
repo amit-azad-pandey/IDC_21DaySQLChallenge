a6132872902866ff50a1e3758e9f4b12297ce758
amit pandey <35841970+amit-azad-pandey@users.noreply.github.com>
2025-11-17 20:47:30 +0530

    Add files via upload

---
+++ Day11_21DaysSQLChallenge.sql
@@ -0,0 +1,18 @@
+-- Practice Questions:
+-- 1. List all unique services in the patients table.
+SELECT DISTINCT service FROM patients;
+
+-- 2. Find all unique staff roles in the hospital.
+SELECT DISTINCT role FROM staff; 
+
+-- 3. Get distinct months from the services_weekly table.
+SELECT DISTINCT month FROM services_weekly;
+
+-- Daily Challenge:
+-- Question: Find all unique combinations of service and event type from the services_weekly table where events are not null or none, along with the count of occurrences for each combination. Order by count descending.
+SELECT DISTINCT service, event, COUNT(*) AS cnt
+FROM services_weekly
+WHERE event IS NOT NULL 
+AND event <> 'None'
+GROUP BY service, event
+ORDER BY cnt DESC;
